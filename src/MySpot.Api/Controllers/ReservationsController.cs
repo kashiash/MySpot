921bb4bcc204e0ddd178d9cdea9a95ba7931a687
Jacek Kosinski <kashiash@gmail.com>
2023-02-28 15:57:43 +0100

    return BadRequest();

--- src/MySpot.Api/Controllers/ReservationsController.cs
+++ src/MySpot.Api/Controllers/ReservationsController.cs
@@ -7,14 +7,63 @@ namespace myspot.api.Controllers;
 [Route(template:"reservations")]
 public class ReservationsController : ControllerBase
 {
-    [HttpGet]
-    public void Get()
+    private static int _id = 1;
+    private static readonly List<Reservation> Reservations = new();
+    private readonly List<string> parkingSpotNames = new()
     {
-    }
+        "P1", "P2","P3","P4","P5"
+    };
+
+    [HttpGet( "{id:int}")]
+
+    public ActionResult<Reservation> Get(int id)
+    {
+       var reservation =  Reservations.SingleOrDefault(x => x.Id == id);
+       if (reservation is null)
+       {
+           HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+           return default;
+       }
+
+       return reservation;
+    } 
+    
+    
+    [HttpGet]
+    public IEnumerable<Reservation> GetAll() => Reservations;
+
+    // public int? Create(Reservation reservation)
+    // {
+    //     var now = DateTime.UtcNow.Date;
+    //     var pastDays = now.DayOfWeek is DayOfWeek.Sunday ? 7 : (int)now.DayOfWeek;
+    //     var reamingDays = 7 - pastDays;
+    //     
+    // }
 
     [HttpPost]
-    public void Post(Reservation reservation)
+    public ActionResult Post(Reservation reservation)
     {
+        if (parkingSpotNames.All(x => x != reservation.ParkingSpotName))
+        {
+            HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            return BadRequest();
+        }
+
+        reservation.Date = DateTime.UtcNow.AddDays(1).Date;
+
+        var reservationAlreadyExists = Reservations.Any(x =>
+            x.ParkingSpotName == reservation.ParkingSpotName && x.Date.Date == reservation.Date.Date);
+
+        if (reservationAlreadyExists)
+        {
+            return BadRequest();
+        }
+        
+        
+        
+        reservation.Id = _id;
+        Reservations.Add(reservation);
+        _id++;
     }
 
 }
--- src/MySpot.Api/Entities/Reservation.cs
+++ src/MySpot.Api/Entities/Reservation.cs
@@ -2,6 +2,7 @@ namespace MySpot.Api.Models;
 
 public class Reservation
 {
+    public int Id { get; set; }
     public string EmployeeName { get; set; }
     public string ParkingSpotName { get; set; }
     public string LicensePlate { get; set; }
